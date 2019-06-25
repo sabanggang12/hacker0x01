c0bf0a68b4da50a2969265860d95e08abec71803
Steven Julian <43600857+sabanggang12@users.noreply.github.com>
2019-06-25 08:24:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+<head><title>SUBDOMAIN TAKEOVER BY steven_julian22</title></head>
+<body>
+<script>alert(document.domain)</script>
+<center><font color=red; size=72px>Subdomain takeover by steven_julian22</font></center>
+sn22shop@gmail.com / +62 89658610626
